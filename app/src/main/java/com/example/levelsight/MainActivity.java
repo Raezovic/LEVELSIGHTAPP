e404f5f98960a60597d06a027180f1bb7d763427
Gichoramike <mikegichora4@gmail.com>
2024-02-06 15:19:27 +0300

    updated layout

--- app/src/main/java/com/example/levelsight/MainActivity.java
+++ app/src/main/java/com/example/levelsight/MainActivity.java
@@ -1,145 +1,13 @@
 package com.example.levelsight;//package com.example.levelsight;
-//
-//
-//import android.content.ActivityNotFoundException;
-//import android.content.Intent;
-//import android.hardware.Camera;
-//import android.os.Bundle;
-//import android.speech.RecognizerIntent;
-//import android.util.Log;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.FrameLayout;
-//import android.widget.Toast;
-//
-//import com.google.android.gms.maps.GoogleMap;
-//import com.google.android.gms.maps.OnMapReadyCallback;
-//import com.google.android.gms.maps.SupportMapFragment;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import java.util.ArrayList;
-//
-//public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
-//
-//    private Camera mCamera;
-//    private CameraPreview mPreview;
-//
-//    Button captureButton;
-//    EditText location_input;
-//
-//
-//
-//
-//    private static final int SPEECH_REQUEST_CODE = 1;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-//
-//        Button voiceAssistantButton = findViewById(R.id.voiceAssistantButton);
-//        location_input = findViewById(R.id.location_input);
-//
-//        voiceAssistantButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startVoiceRecognition();
-//            }
-//        });
-//
-//
-//
-////        mCamera = getCameraInstance();
-//        mPreview = new CameraPreview(this, mCamera);
-//
-//        mCamera = getCameraInstance();
-//
-//        if (mCamera != null) {
-//            mPreview = new CameraPreview(this, mCamera);
-//
-//            FrameLayout previewLayout = findViewById(R.id.cameraPreview);
-//            previewLayout.addView(mPreview);
-//
-//            captureButton = findViewById(R.id.captureButton);
-//            captureButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    mCamera.takePicture(null, null, mPictureCallback);
-//                }
-//            });
-//        } else {
-//            // Handle the case where the camera cannot be opened
-//            Toast.makeText(this, "Failed to open camera", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//    private void startVoiceRecognition() {
-//        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-//
-//        try {
-//            startActivityForResult(intent, SPEECH_REQUEST_CODE);
-//        } catch (ActivityNotFoundException e) {
-//            Toast.makeText(this, "Speech recognition not supported on this device", Toast.LENGTH_SHORT).show();
-//        }
-//    }
-//
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-//            // Handle the speech recognition results
-//            ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-//            String spokenText = results.get(0);
-//
-//            // Do something with the spoken text
-//            Toast.makeText(this, "You said: " + spokenText, Toast.LENGTH_SHORT).show();
-//            location_input.setText(spokenText);
-//        }
-//    }
-//
-//
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        // add map pointers etc
-//    }
-//
-//
-//
-//    // Camera initialization method
-//    private Camera getCameraInstance() {
-//        Camera c = null;
-//        try {
-//            c = Camera.open();
-//        } catch (Exception e) {
-//            // Log the exception for debugging
-//            Log.e("CameraPreview", "Error opening camera: " + e.getMessage());
-//            e.printStackTrace();
-//        }
-//        return c;
-//    }
-//
-//
-//
-//    // Picture callback for handling captured photos
-//    private Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
-//        @Override
-//        public void onPictureTaken(byte[] data, Camera camera) {
-//            // Process the captured photo data
-//        }
-//    };
-//
-//    // Other necessary lifecycle methods, e.g., release the camera in onDestroy()
-//}
+
+
+import static android.content.ContentValues.TAG;
 
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -153,18 +21,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -177,8 +50,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private LocationManager locationManager;
 
     private static final int SPEECH_REQUEST_CODE = 1;
+    private static final int  REQUEST_IMAGE_CAPTURE = 2;
+
+
 
     EditText location_input;
+
+    ImageView capturedImageView;
+
+    // AzureImageAnalyzer instance
+    private AzureImageAnalyzer imageAnalyzer;
+    private Camera mCamera;
+    private CameraPreview mPreview;
 
 
     @Override
@@ -194,12 +77,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         EditText locationInput = findViewById(R.id.location_input);
         Button navigateButton = findViewById(R.id.navigateButton);
 
-
-
+        //computer vision code
+        AzureImageAnalyzer imageAnalyzer = new AzureImageAnalyzer(this);
 
 
         Button voiceAssistantButton = findViewById(R.id.voiceAssistantButton);
         location_input = findViewById(R.id.location_input);
+
+        // Check camera permission
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, REQUEST_IMAGE_CAPTURE);
+        } else {
+            // Create an instance of Camera
+            mCamera = getCameraInstance();
+            // Set camera rotation to 270 degrees counterclockwise
+            mCamera.setDisplayOrientation(90);
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = findViewById(R.id.cameraPreview);
+            preview.addView(mPreview);
+        }
+
+        //  capturedImageView = findViewById(R.id.capturedImageView);
 
         voiceAssistantButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -227,7 +126,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     LatLng currentLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
                     setupMap(currentLocation, destinationLocation);
                     openNavigation(currentLocation, destinationLocation);
-                } else {
+                    // Call Azure API for image analysis
+
+
+                    imageAnalyzer.analyzeImage("C:\\Users\\Lenovo\\Downloads\\streetbulb.jpeg", new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            // Handle the API response
+                            Log.d(AzureImageAnalyzer.TAG, "API Response: " + response.toString());
+                            //Log.d(TAG, "API Response: " + response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // Handle errors
+                            Log.e(AzureImageAnalyzer.TAG, "API Error: " + error.toString());
+                           // Log.e(TAG, "API Error: " + error.toString());
+                        }
+                    });
+                }
+                else {
                     Toast.makeText(MainActivity.this, "Unable to retrieve current location", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -236,6 +154,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 
     }
+
+
+
+
+    private Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+            e.printStackTrace();
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    // Picture callback for handling captured photos
+    private Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // Process the captured photo data
+        }
+    };
 
     private void openNavigation(LatLng currentLocation, String destinationLocation) {
         Geocoder geocoder = new Geocoder(this, Locale.getDefault());
